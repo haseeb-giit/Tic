f976178ea3d9851c569702c59b25c3504ab40363
Haseeb Ali <59574579+SN-Point@users.noreply.github.com>
2023-07-02 15:07:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ let gameover = new Audio("gameover.mp3")
 let turn = "X"
 let isgameover = false;
 var won = new Audio();
-won.src="won.wav";
+var losss = new Audio();
+won.src = "won.wav";
+losss.src = "loss.wav"
+var draw_game = 0;
 // music.play();
 
 // Function to change the turn
-const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
 }
+$(".draw").hide();
 
 // Function to check for a win
-const checkWin = ()=>{
+const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
         [0, 1, 2, 5, 5, 0],
@@ -25,11 +29,12 @@ const checkWin = ()=>{
         [0, 4, 8, 5, 15, 45],
         [2, 4, 6, 5, 15, 135],
     ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")) {
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
             isgameover = true
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "500px";
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "100px";
+            draw_game--;
             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
             document.querySelector(".line").style.width = "20vw";
             won.play();
@@ -40,31 +45,46 @@ const checkWin = ()=>{
 // Game Logic
 // music.play()
 let boxes = document.getElementsByClassName("boxxx");
-Array.from(boxes).forEach(element =>{
+Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText === ''){
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
             boxtext.innerText = turn;
             turn = changeTurn();
             audioTurn.play();
             checkWin();
-            if (!isgameover){
-                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            } 
+            if (!isgameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
         }
     })
 })
 
 // Add onclick listener to reset button
-reset.addEventListener('click', ()=>{
+reset.addEventListener('click', () => {
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
         element.innerText = ""
     });
-    turn = "X"; 
+    turn = "X";
     isgameover = false
     document.querySelector(".line").style.width = "0vw";
-    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
 })
+$(".g_reset").click(function () {
+    draw_game=0;
+    $(".draw").hide();
+    document.getElementById("sad").style.width = "0";
+});
 
+$(".boxxx").click(function () {
+    draw_game++;
+    console.log(draw_game);
+    if (draw_game == 9) {
+        console.log("Draw Game");
+        document.getElementById("sad").style.width = "200px";
+        losss.play();
+        $(".draw").show();
+    }
+});
